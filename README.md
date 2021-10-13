cb78739e3c90efa94750b5afecc873b801264e91
xingpengchao <2659489932@qq.com>
2021-10-13 19:04:00 +0800

    feat: 代码更新'

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## 前端日常开发工具类整理
 
-### 目前收录工具类目录展示
+### 目录展示
 
 - 身份证脱敏显示
 - 身份证校验
@@ -11,7 +11,7 @@
 - 节流
 - Web 存储封装
 - 数字转大写金额
-- [判断是否为 ios](https://github.com/RangiLyu/nanodet)
+- [判断是否为 ios](https://github.com/xingpengchao/Utils/blob/main/util.js)
 - 判断是否为 PC 端
 - 格式化时间(兼容 ios)
 - 获取 url 参数转换为 json 对象
--- util.js
+++ util.js
@@ -101,7 +101,7 @@ function throttle (func, wait, type) {
   };
 }
 
- /* web存储封装 */
+/* web存储封装 */
 class StorageFn {
   constructor () {
       this.ls = window.localStorage;
@@ -203,7 +203,6 @@ class StorageFn {
       this.ss.clear()
   }
 }
-
 
 /* 将数字转换为大写金额 */
 function changeToChinese (Num) {
@@ -355,49 +354,6 @@ function changeToChinese (Num) {
     }
   }
   return newchar;
-}
-
-/* 将阿拉伯数字翻译成中文的大写数字 */
-function numberToChinese (num) {
-  const AA = new Array("零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十");
-  const BB = new Array("", "十", "百", "仟", "萬", "億", "点", "");
-  let a = ("" + num).replace(/(^0*)/g, "").split("."),
-      k = 0,
-      re = "";
-  for(let i = a[0].length - 1; i >= 0; i--) {
-    switch(k) {
-      case 0:
-        re = BB[7] + re;
-        break;
-      case 4:
-        if(!new RegExp("0{4}//d{" + (a[0].length - i - 1) + "}$")
-          .test(a[0]))
-          re = BB[4] + re;
-        break;
-      case 8:
-        re = BB[5] + re;
-        BB[7] = BB[5];
-        k = 0;
-        break;
-    }
-    if(k % 4 == 2 && a[0].charAt(i + 2) != 0 && a[0].charAt(i + 1) == 0)
-      re = AA[0] + re;
-    if(a[0].charAt(i) != 0)
-      re = AA[a[0].charAt(i)] + BB[k % 4] + re;
-    k++;
-  }
-
-  // 加上小数部分(如果有小数部分)
-  if(a.length > 1) {
-    re += BB[6];
-    for(let i = 0; i < a[1].length; i++)
-        re += AA[a[1].charAt(i)];
-  }
-  if(re == '一十')
-      re = "十";
-  if(re.match(/^一/) && re.length == 3)
-      re = re.replace("一", "");
-  return re;
 }
 
 /* 判断是否为Ios */
