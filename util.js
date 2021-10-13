dac1fd2d2cb298581f08b70e8ea614f4abe5b4c8
xingpengchao <2659489932@qq.com>
2021-10-13 18:27:31 +0800

    feat: 添加深拷贝

--- util.js
+++ util.js
@@ -460,24 +460,54 @@ function formatTime(val, format) {
   }
 };
 
-
 /* 获取全部url参数,并转换成json对象 */
 function getUrlAllParams (url) {
   let _url = url ? url : window.location.href;
   let _pa = _url.substring(_url.indexOf('?') + 1),
-      _arrS = _pa.split('&'),
-      _rs = {};
+    _arrS = _pa.split('&'),
+    _rs = {};
   for (let i = 0, _len = _arrS.length; i < _len; i++) {
-      let pos = _arrS[i].indexOf('=');
-      if (pos == -1) {
-          continue;
-      }
-      let name = _arrS[i].substring(0, pos),
-          value = window.decodeURIComponent(_arrS[i].substring(pos + 1));
-      _rs[name] = value;
+    let pos = _arrS[i].indexOf('=');
+    if (pos == -1) {
+        continue;
+    }
+    let name = _arrS[i].substring(0, pos),
+        value = window.decodeURIComponent(_arrS[i].substring(pos + 1));
+    _rs[name] = value;
   }
   return _rs;
 }
+
+/* 深拷贝 */
+const deepClone = (target, cache = new Map()) => {
+  const isObject = (obj) => typeof obj === 'object' && obj !== null
+
+  if (isObject(target)) {
+    // 解决循环引用
+    const cacheTarget = cache.get(target)
+    // 已经存在直接返回，无需再次解析
+    if (cacheTarget) {
+      return cacheTarget
+    }
+
+    let cloneTarget = Array.isArray(target) ? [] : {}
+
+    cache.set(target, cloneTarget)
+
+    for (const key in target) {
+      if (target.hasOwnProperty(key)) {
+        const value = target[ key ]
+        cloneTarget[ key ] = isObject(value) ? deepClone(value, cache) : value
+      }
+    }
+
+    return cloneTarget
+  } else {
+    return target
+  }
+}
+
+
 
 
 
